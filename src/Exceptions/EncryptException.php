7c161e4cc8544e08f24ca73bcedaa12b3a9e0451
Eric Lamb <eric@mithra62.com>
2016-04-14 16:37:25 -0700

    Adds exception

---
+++ src/Exceptions/EncryptException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * mithra62
+ *
+ * @author		Eric Lamb <eric@mithra62.com>
+ * @copyright	Copyright (c) 2015, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/
+ * @version		1.0
+ * @filesource 	./mithra62/Exceptions/EncryptException.php
+ */
+namespace mithra62\Exceptions;
+
+use mithra62\Exception;
+
+/**
+ * mithra62 - Encryption Exception Object
+ *
+ * Handles exceptions from the Encryption object
+ *
+ * @package Exceptions
+ * @author Eric Lamb <eric@mithra62.com>
+ */
+class EncryptException extends Exception
+{
+}
